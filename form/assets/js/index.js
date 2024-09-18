b99257120e6680c3c49e58c08d1016e8221acb48
mrmuhammadrifki <immuhammadrifki@gmail.com>
2024-09-18 09:58:11 +0700

    feat(form): implement oninput, onfocus, and onblur event

--- form/assets/js/index.js
+++ form/assets/js/index.js
@@ -1,10 +1,36 @@
-const submitAction = document.getElementById("formDataDiri");
+document.addEventListener("DOMContentLoaded", function () {
+  const inputMaxLengthOnLoad = document.getElementById("inputNama").maxLength;
+  document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
 
-submitAction.addEventListener("submit", function (event) {
-  const inputNama = document.getElementById("inputNama").value;
-  const inputDomisili = document.getElementById("inputDomisili").value;
-  const hiddenMessage = `Halo, ${inputNama}. Bagaimana kabar kamu dan keluarga di ${inputDomisili}`;
+  // oninput
+  document.getElementById("inputNama").addEventListener("input", function () {
+    const jumlahKarakterDiketik = document.getElementById("inputNama").value.length;
+    const jumlahKarakterMaksimal = document.getElementById("inputNama").maxLength;
 
-  document.getElementById("messageAfterSubmit").innerText = hiddenMessage;
-  event.preventDefault();
+    console.log("Jumlah karakter diketik: " + jumlahKarakterDiketik);
+    console.log("Jumlah karakter maksimal: " + jumlahKarakterMaksimal);
+
+    const sisaKarakterUpdate = jumlahKarakterMaksimal - jumlahKarakterDiketik;
+    document.getElementById("sisaKarakter").innerText = sisaKarakterUpdate.toString();
+
+    if (sisaKarakterUpdate == 0) {
+      document.getElementById("sisaKarakter").innerText = "Anda mencapai batas karakter!";
+    } else if (sisaKarakterUpdate <= 5) {
+      document.getElementById("notifikasiSisaKarakter").style.color = "red";
+    } else {
+      document.getElementById("notifikasiSisaKarakter").style.color = "black";
+    }
+  });
+
+  // onfocus
+  document.getElementById("inputNama").addEventListener("focus", function () {
+    console.log("Input nama sedang fokus");
+    document.getElementById("notifikasiSisaKarakter").style.visibility = "visible";
+  });
+
+  // onblur
+  document.getElementById("inputNama").addEventListener("blur", function () {
+    console.log("Input nama tidak fokus lagi");
+    document.getElementById("notifikasiSisaKarakter").style.visibility = "hidden";
+  });
 });
